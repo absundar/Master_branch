e5c886308d2c36dc29dd7d9e3bff93ba001323ec
Abhinaya Sundar <abhinaya5sundar@gmail.com>
2025-02-20 21:27:14 +0530

    test

---
+++ test/src/main/java/DB/test/App.java
@@ -0,0 +1,13 @@
+package DB.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
