2f8be857acf90034c9b1e5250c823ebcb19ea15a
Abhinaya Sundar <abhinaya5sundar@gmail.com>
2025-02-20 20:30:27 +0530

    CucumberWithSelenium

---
+++ CucumberWithSelenium/Features/MyTest.feature
@@ -0,0 +1,14 @@
+Feature: Open google.com feature 
+
+
+Scenario: open google website and search
+
+
+Given user is entering google.com			
+
+
+When user clicks the search box			
+
+And types Abhi and hit search 
+
+Then the user should see the results for Abhi		
---
+++ CucumberWithSelenium/src/runnerClass/RunnerClass.java
@@ -0,0 +1,16 @@
+package runnerClass;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Features/MyTest.feature", glue="stepDefinitions")
+public class RunnerClass {
+
+	
+
+	}
+
+
