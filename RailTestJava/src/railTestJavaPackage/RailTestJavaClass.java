8d343ba2c309a6f026261277f55211888ba1f4fc
Abhinaya Sundar <abhinaya5sundar@gmail.com>
2025-02-20 21:18:35 +0530

    selenium practice

---
+++ RailTestJava/src/railTestJavaPackage/RailTestJavaClass.java
@@ -0,0 +1,11 @@
+package railTestJavaPackage;
+
+import org.openqa.selenium.WebDriver;
+
+public class RailTestJavaClass {
+
+	public static void main(String[] args) {
+WebDriver driver = new Chrome
+	}
+
+}
